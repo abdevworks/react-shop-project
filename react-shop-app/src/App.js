aa1e6aad657575bea7cf9e41b6efe682dd9242be
Mal <czasacz@gmail.com>
2021-02-09 10:49:58 +0100

    Created Basic Nav.

--- react-shop-app/src/App.js
+++ react-shop-app/src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
 import StoreFront from "./StoreFront.js";
+import Home from "./Home.js";
+import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
 
 
 function App() {
   
   return (
-    <StoreFront />
+
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/sf">Store Front</Link>
+          </li>
+          <li>
+            <Link to="sl">Shop List</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/sf">
+          <StoreFront />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
   );
 }
 
